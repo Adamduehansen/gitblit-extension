038a07636824c1fff013847177a40195d426f7a7
Adam Due Hansen <adamduehan@gmail.com>
2023-08-07 12:22:49 +0200

    Popup Preact component now replicates open functionality.

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,57 +1,42 @@
-import { render } from 'preact';
+import { JSX, render } from 'preact';
 import { useState } from 'preact/hooks';
+import { Ticket } from './services/TicketService';
 
-function Popup() {
-  const [count, setCount] = useState(0);
+function Popup(): JSX.Element {
+  async function onOpenAllClicked(): Promise<void> {
+    const { tickets } = (await chrome.storage.local.get('tickets')) as {
+      tickets: Ticket[];
+    };
+
+    let tabIds: number[] = [];
+    for (const ticket of tickets) {
+      const tab = await chrome.tabs.create({
+        url: ticket.jsonUrl,
+        active: false,
+      });
+
+      if (tab.id === undefined) {
+        return;
+      }
+
+      tabIds.push(tab.id);
+    }
+
+    const tabGroup = await chrome.tabs.group({
+      tabIds: tabIds,
+    });
+
+    chrome.tabGroups.update(tabGroup, {
+      title: 'Gitblit Extension Group',
+      collapsed: true,
+    });
+  }
 
   return (
     <div>
-      <button
-        onClick={() => {
-          setCount((current) => current + 1);
-        }}
-      >
-        Count
-      </button>
-      <p>You've counted {count} times!</p>
+      <button onClick={onOpenAllClicked}>Open all</button>
     </div>
   );
 }
 
 render(<Popup />, document.getElementById('root')!);
-
-// import {
-//   Ticket,
-//   TicketRepository,
-//   TicketService,
-// } from './services/TicketService';
-
-// const button = document.querySelector('button');
-// button?.addEventListener('click', async () => {
-//   const ticketService = new TicketService(TicketRepository);
-//   const { tickets } = (await chrome.storage.local.get('tickets')) as {
-//     tickets: Ticket[];
-//   };
-
-//   let tabIds: number[] = [];
-//   for (const ticket of tickets) {
-//     const tab = await chrome.tabs.create({
-//       url: ticket.jsonUrl,
-//       active: false,
-//     });
-
-//     if (tab.id === undefined) {
-//       return;
-//     }
-
-//     tabIds.push(tab.id);
-//   }
-
-//   const tabGroup = await chrome.tabs.group({
-//     tabIds: tabIds,
-//   });
-
-//   chrome.tabGroups.update(tabGroup, {
-//     title: 'Gitblit Extension Group',
-//   });
-// });
