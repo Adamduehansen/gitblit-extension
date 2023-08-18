abd3e05e5165d9a3cf599ffcb65746a9c85d0bfd
Adam Due Hansen <adamduehan@gmail.com>
2023-08-18 07:25:01 +0200

    Adds tests for methods in NotificationService.

--- src/services/NotificationService.test.ts
+++ src/services/NotificationService.test.ts
@@ -1,5 +1,6 @@
 import {
   CreateNotification,
+  Notification,
   NotificationRepository,
   NotificationService,
 } from './NotificationService';
@@ -27,6 +28,124 @@ describe('NotificationService', () => {
 
       // Assert
       expect(testRepository.setNotifications).toHaveBeenCalled();
+    });
+  });
+
+  describe('createNotifications', () => {
+    test('should create multiple notifications at onw time', async () => {
+      // Arrange
+      const testRepository: NotificationRepository = {
+        getNotifications: async () => [],
+        setNotifications: jest.fn(),
+      };
+
+      const notificationService = new NotificationService(testRepository);
+
+      // Act
+      await notificationService.createNotifications([]);
+
+      // Assert
+      expect(testRepository.setNotifications).toHaveBeenCalled();
+    });
+  });
+
+  describe('setPushed', () => {
+    test('should set a notification as pushed', async () => {
+      // Arrange
+      const notificationInStorage: Notification = {
+        id: 'any-id',
+        title: 'any-title',
+        message: 'any-message',
+        pushed: false,
+        read: false,
+        ticketRepository: 'any-repository',
+        ticketNumber: 1,
+      };
+      let notificationsInStorage: Notification[] = [notificationInStorage];
+      const testRepository: NotificationRepository = {
+        getNotifications: async () => notificationsInStorage,
+        setNotifications: async (newNotifications) => {
+          notificationsInStorage = newNotifications;
+        },
+      };
+
+      const notificationService = new NotificationService(testRepository);
+
+      // Act
+      await notificationService.setPushed('any-id');
+
+      // Assert
+      expect(notificationInStorage.pushed).toEqual(true);
+    });
+  });
+
+  describe('setRead', () => {
+    test('should set a notifaction as read', async () => {
+      // Arrange
+      const notificationInStorage: Notification = {
+        id: 'any-id',
+        title: 'any-title',
+        message: 'any-message',
+        pushed: false,
+        read: false,
+        ticketRepository: 'any-repository',
+        ticketNumber: 1,
+      };
+      let notificationsInStorage: Notification[] = [notificationInStorage];
+      const testRepository: NotificationRepository = {
+        getNotifications: async () => notificationsInStorage,
+        setNotifications: async (newNotifications) => {
+          notificationsInStorage = newNotifications;
+        },
+      };
+
+      const notificationService = new NotificationService(testRepository);
+
+      // Act
+      await notificationService.setRead('any-id');
+
+      // Assert
+      expect(notificationInStorage.read).toEqual(true);
+    });
+  });
+
+  describe('getNotifications', () => {
+    test('should return all notifications in storage', async () => {
+      // Arrange;
+      let notificationsInStorage: Notification[] = [
+        {
+          id: 'any-id-1',
+          title: 'any-title-1',
+          message: 'any-message-1',
+          pushed: false,
+          read: false,
+          ticketRepository: 'any-repository-1',
+          ticketNumber: 1,
+        },
+        {
+          id: 'any-id-2',
+          title: 'any-title-2',
+          message: 'any-message-2',
+          pushed: false,
+          read: false,
+          ticketRepository: 'any-repository-2',
+          ticketNumber: 1,
+        },
+      ];
+      const testRepository: NotificationRepository = {
+        getNotifications: async () => notificationsInStorage,
+        setNotifications: async (newNotifications) => {
+          notificationsInStorage = newNotifications;
+        },
+      };
+
+      const notificationService = new NotificationService(testRepository);
+
+      // Act
+      const notifications = await notificationService.getNotifications();
+
+      // Assert
+      expect(notifications).toEqual(notificationsInStorage);
     });
   });
 });
